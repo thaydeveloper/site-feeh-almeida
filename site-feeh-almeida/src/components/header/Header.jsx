2ac6cd3d78b97c47d1d6a1b7450138ef0aafc671
thaydeveloper <thaydeveloper26@gmail.com>
2023-03-14 16:04:32 -0300

    first commit

---
+++ site-feeh-almeida/src/components/header/Header.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./header.css";
+
+const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Fernanda
+        </a>
+
+        <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="#home" className="nav__link active-link">
+                <i className="uil uil-home nav__icon"></i> Home
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> Sobre
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#scheduling" className="nav__link">
+                <i className="uil uil-calender nav__icon"></i> Agendamentos
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i>
+                Contato
+              </a>
+            </li>
+          </ul>
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => setToggle(!toggle)}
+          ></i>
+        </div>
+
+        <div className="nav__toggle" onClick={() => setToggle(!toggle)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
