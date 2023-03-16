af835f6198507650627bc8988ef2b1f2c9095bd1
thaydeveloper <thaydeveloper26@gmail.com>
2023-03-16 13:39:27 -0300

    criado o componente de home

---
+++ site-feeh-almeida/src/components/home/Social.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social ">
+      <a
+        href="https://www.instagram.com/nails_fernandaalmeidajf/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.facebook.com/fernanda.almeida.96343"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-facebook"></i>
+      </a>
+      <a
+        href="https://wa.me/5532998111879?text=Ol%C3%A1+Sou+Feeh+Almeida+Nails+design"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-whatsapp"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
