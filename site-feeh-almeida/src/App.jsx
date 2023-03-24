95d2743ed51e89eb472ffc98663fb4d92a1b3610
thaydeveloper <thaydeveloper26@gmail.com>
2023-03-24 13:25:50 -0300

    feito a animacao de rolagem dos componentes about e gallery

--- site-feeh-almeida/src/App.jsx
+++ site-feeh-almeida/src/App.jsx
@@ -3,14 +3,30 @@ import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Gallery from "./components/galery/Gallery";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 function App() {
+  const [ref, inView] = useInView({
+    threshold: 0.1,
+    triggerOnce: false,
+    rootMargin: "-100px 0px",
+  });
   return (
     <>
       <Header />
+
       <main className="main">
         <Home />
-        <About />
+        <motion.div
+          ref={ref}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: inView ? 1 : 0 }}
+          transition={{ duration: 1.5 }}
+        >
+          <About />
+        </motion.div>
+
         <Gallery />
       </main>
     </>
--- site-feeh-almeida/src/components/galery/Gallery.jsx
+++ site-feeh-almeida/src/components/galery/Gallery.jsx
@@ -1,45 +1,45 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./gallery.css";
-import { motion } from "framer-motion";
+import { motion, useInView, useAnimate } from "framer-motion";
+/* import { useInView } from "react-intersection-observer"; */
 import bestImages from "../../services/bestImages";
 
 const Gallery = () => {
-  const carousel = useRef();
   const [width, setWidth] = useState();
+  const carousel = useRef();
+  const [scope, animate] = useAnimate();
+  const isInView = useInView(carousel);
 
   useEffect(() => {
     setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth);
-  }, []);
+    if (isInView) {
+      animate(scope.current, { x: [300, 0] }, { duration: 2 });
+      return;
+    }
+  }, [isInView]);
   return (
-    <section className="section__gallery container grid" id="gallery">
-      <div className="container__gallery grid">
-        <div className="content__gallery">
+    <section className="gallery section" id="gallery">
+      <h1 className="title__gallery ">Galeria</h1>
+      <div className="content__gallery container">
+        <motion.div
+          ref={carousel}
+          style={{ overflow: "scroll" }}
+          whileTap={{ cursor: "grabbing" }}
+          className="carousel container"
+        >
           <motion.div
-            ref={carousel}
-            whileTap={{ cursor: "grabbing" }}
-            className="carousel container"
+            ref={scope}
+            className="img-container"
+            dragConstraints={{ right: 0, left: -width }}
+            drag="x"
           >
-            <h1 className="title__gallery">Galeria</h1>
-            <motion.div
-              className="img-container"
-              dragConstraints={{ right: 0, left: -width }}
-              drag="x"
-              initial={{ x: 300 }}
-              animate={{ x: 0 }}
-              transition={{ duration: 1 }}
-            >
-              {bestImages.map((bestImage) => (
-                <motion.div className="item" key={bestImage.image}>
-                  <img src={bestImage.image} alt="imagens-de-unhas-de-fibra" />
-                </motion.div>
-              ))}
-            </motion.div>
+            {bestImages.map((bestImage) => (
+              <motion.div className="item" key={bestImage.image}>
+                <img src={bestImage.image} alt="imagens-de-unhas-de-fibra" />
+              </motion.div>
+            ))}
           </motion.div>
-        </div>
-
-        {/* <div className="content__results">
-          <h1 className="title__gallery">Antes x Depois</h1>
-        </div> */}
+        </motion.div>
       </div>
     </section>
   );
