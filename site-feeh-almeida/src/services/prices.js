9dfe1345a68cfcfb75ca4aa4e0a857ddd0b3e48e
thaydeveloper <thaydeveloper26@gmail.com>
2023-10-04 13:16:28 -0300

    adicionado novos servicoes na aplicacao e a duracao de cada uma deles

--- site-feeh-almeida/src/services/prices.js
+++ site-feeh-almeida/src/services/prices.js
@@ -3,47 +3,81 @@ export const tablePrices = [
     title: "Alongamento Fibra Encapsulada",
     id: 1,
     price: " R$ 135,00",
+    durance: 2.3,
   },
   {
     title: "Alongamento Fibra Comum",
     id: 2,
     price: " R$ 110,00",
+    durance: 2.3,
   },
+  {
+    title: "Alongamento soft gel",
+    id: 13,
+    price: " R$ 110,00",
+    durance: 1.3,
+  },
+  {
+    title: "Alongamento molde russo",
+    id: 12,
+    price: " R$ 110,00",
+    durance: 2.0,
+  },
+
   {
     title: "Banho De Gel",
     id: 3,
     price: " R$ 90,00",
+    durance: 1.3,
+  },
+  {
+    title: "Blindagem ",
+    id: 11,
+    price: " R$ 90,00",
+    durance: 1.0,
+  },
+  {
+    title: "Blindagem manutenção ",
+    id: 14,
+    price: " R$ 90,00",
+    durance: 1.0,
   },
   {
     title: "Esmaltação Em Gel",
     id: 4,
     price: " R$ 35,00",
+    durance: 2.3,
   },
 
   {
     title: "Manutenção Banho De Gel",
     id: 6,
     price: " R$ 60,00",
+    durance: 1.3,
   },
   {
     title: "Manutenção Fibra 15-20 Dias",
     id: 7,
     price: " R$ 85,00",
+    durance: 1.3,
   },
   {
     title: "Manutenção Fibra 20-30 Dias",
     id: 8,
     price: " R$ 95,00",
+    durance: 1.3,
   },
   {
     title: "Manutenção Encapsulada 15-20 Dias",
     id: 9,
     price: " R$ 100,00",
+    durance: 1.3,
   },
   {
     title: "Manutenção Encapsulada 20-30 Dias",
     id: 10,
     price: " R$ 115,00",
+    durance: 1.3,
   },
 ];
 
@@ -52,20 +86,24 @@ export const tableAdditionalServices = [
     title: "Pé",
     id: 1,
     price: " R$ 26,00",
+    durance: 1.0,
   },
   {
     title: "Esmaltação Em Gel",
     id: 2,
     price: " R$ 35,00",
+    durance: 1.0,
   },
   {
     title: "Decoração à partir",
     id: 3,
     price: " R$ 20,00",
+    durance: 0.3,
   },
   {
     title: "Mão Comum ",
     id: 4,
     price: " R$ 24,00",
+    durance: 0.4,
   },
 ];
